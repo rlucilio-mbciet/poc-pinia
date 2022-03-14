a4f839b9196be8a615f18d7c667ec7851954b593
rlucilio-mbciet <97033997+rlucilio-mbciet@users.noreply.github.com>
2022-03-14 13:17:01 -0300

    Remove files

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import { userStore } from "./userStore";
 
+//Store main agrupando por ns
 export const useStore = defineStore("main", {
   state: () => ({
     users: userStore(),
